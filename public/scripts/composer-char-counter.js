9cdc4d70e98a568e2b5e93d7c8b9fc015a32354e
egomatsushita <eduardo.go.rj@gmail.com>
2017-07-12 13:19:29 +0000

    Change variable name compose_length to message length, and also add a comment

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,11 +4,15 @@ $(document).ready(function() {
 
   $(".new-tweet").on('keyup', "textarea", function() {
 
-    let compose_length = $(this).val().length;
+    let message_length = $(this).val().length;
     let counter = $(this).closest(".new-tweet").find(".counter");
     let counter_length = +counter.text();
 
-    counter.text(counter_start - compose_length);
+    counter.text(counter_start - message_length);
+
+    // If message contains more than 140 characters,
+    // Change the color of the counter to red when it's negative,
+    // Otherwise keep it black.
     if (counter.text() < 0) {
       counter.css({'color': 'red'});
     } else {
